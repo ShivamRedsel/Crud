655451ac9046bf71e194ebb818536450e4f8f33b
Shivam Rai <146408282+ShivamRedsel@users.noreply.github.com>
2023-10-04 14:26:32 +0530

    Add files via upload

---
+++ Product-Management-Crud-App/src/main/java/com/product/ProductManagementCrudAppApplication.java
@@ -0,0 +1,14 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementCrudAppApplication.class, args);
+		System.out.println("chl rha hai");
+	}
+
+}
